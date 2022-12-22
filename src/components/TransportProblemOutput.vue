e29b4738f6b9c3229477d53bda4028f27b497796
Erl777 <erl.pank777@gmail.com>
2022-12-22 13:14:30 +0300

    доработки вывода

--- src/components/TransportProblemOutput.vue
+++ src/components/TransportProblemOutput.vue
@@ -1,31 +1,31 @@
 <template>
-  <div class="table-output">
+  <div class="output">
     <header>
       <div class="row title">
-        <span class="width200p">Доставка</span>
-        <span></span>
-        <span class="width200p">Разгрузка</span>
+        <span>Доставка</span>
+        <span>Разгрузка</span>
       </div>
       <div class="row subtitles">
-        <span class="column-title">Пункт отправления</span>
-        <span class="column-title">Кол-во</span>
-        <span></span>
-        <span class="column-title">Пункт назначения</span>
-        <span class="column-title">Кол-во</span>
+        <span>Пункт отправления</span>
+        <span>Кол-во</span>
+        <span>Пункт назначения</span>
+        <span>Кол-во</span>
       </div>
     </header>
-    <div class="row" v-for="supplier in suppliersGrouping">
+    <div class="row"
+       v-for="supplier in suppliersGrouping"
+       :key="supplier.name"
+    >
 
       <div>
-        <span class="point"> {{ supplier.name }} </span>
+        <span class="bordered"> {{ supplier.name }} </span>
       </div>
-      <span class="vertical-align-center"> {{ supplier.count }} </span>
-      <span class=""></span>
+      <span> {{ supplier.count }} </span>
       <template v-for="recipient in supplier.recipients">
-        <div class="recipient-point" :class="recipient.class">
-          <span class="point"> {{ recipient.name }} </span>
+        <div class="recipient" :class="recipient.class">
+          <span class="bordered"> {{ recipient.name }} </span>
         </div>
-        <span class="vertical-align-center" :class="recipient.class"> {{ recipient.count }} </span>
+        <span :class="recipient.class"> {{ recipient.count }} </span>
       </template>
 
     </div>
@@ -78,8 +78,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$grid-columns: 100px 100px minmax(100px, 1fr) 100px 100px;
 
-.table-output {
+.output {
   padding: 10px;
   border-radius: 10px;
   background-color: #FFFFFF;
@@ -88,26 +89,29 @@ export default {
 
 .row {
   display: grid;
-  grid-template-columns: 100px 100px minmax(100px, 1fr) 100px 100px;;
+  grid-template-columns: $grid-columns;
   padding: 24px 0 18px 0;
   border-bottom: 1px solid #d5d5d5;
   column-gap: 10px;
   row-gap: 10px;
 
+  > span {
+    margin: auto 0;
+  }
+
   &.title {
     border-bottom: none;
-    grid-template-columns: 100px 100px minmax(100px, 1fr) 100px 100px;
+    grid-template-columns: $grid-columns;
     padding-bottom: 18px;
     padding-top: 12px;
 
-    .width200p {
+    span:first-child, span:last-child {
       grid-column-start: span 2;
     }
-  }
 
-  &.subtitles {
-    padding-bottom: 14px;
-    padding-top: 0;
+    span:last-child {
+      grid-column: 4/5 span;
+    }
   }
 
   &:last-child {
@@ -116,24 +120,25 @@ export default {
   }
 }
 
-.point {
+.subtitles {
+  padding-bottom: 14px;
+  padding-top: 0;
+  font-size: 12px;
+  color: #929292;
+
+  & span:nth-child(3) {
+    grid-column: 4;
+  }
+}
+
+.bordered {
   display: inline-block;
   padding: 5px;
   background-color: #dedede;
   border-radius: 4px;
 }
 
-.vertical-align-center {
-  margin: auto 0;
-}
-
-.column-title {
-  font-size: 12px;
-  margin-top: auto;
-  color: #929292;
-}
-
-.recipient-point {
+.recipient {
   grid-column: 4;
 }
 
